760f27f06a1f317e24f8fe490993bd0d12268106
Pieter Vantorre <pietervantorre@gmail.com>
2018-12-02 17:40:57 +0100

    Completed 1-1

---
+++ 2018/go/day1/puzzle1.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/nuclearcookie/aoc2018/day1/input"
+)
+
+func main() {
+	data := input1.Get()
+	scanner := bufio.NewScanner(strings.NewReader(data))
+
+	result := 0
+	for scanner.Scan() {
+		i, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			panic(err)
+		}
+		result += i
+	}
+	fmt.Println("Total frequency offset: ", result)
+}
