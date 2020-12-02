e7c0574ed2dd1dc78d7a6128599bbb5caf58fd7a
Pieter Vantorre <pietervantorre@gmail.com>
2020-12-02 22:20:21 +0100

    Day 1

---
+++ 2020/C#/Day 1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using System.IO;
+using System;
+
+namespace Day_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = File.ReadAllLines("Input/data.txt");
+            var int_data = data.Select(x => int.Parse(x)).ToArray();
+            PartA(int_data);
+            PartB(int_data);
+        }
+
+        private static void PartA(int[] int_data)
+        {
+            Console.WriteLine("PART A:");
+            for (int i = 0; i < int_data.Length; ++i)
+            {
+                var current = int_data[i];
+                for (int j = i + 1; j < int_data.Length; ++j)
+                {
+                    var next = int_data[j];
+                    if (current + next == 2020)
+                    {
+                        Console.WriteLine($"1: {current} 2: {next}. result: {current * next}");
+                        break;
+                    }
+                }
+            }
+        }
+        private static void PartB(int[] int_data)
+        {
+            Console.WriteLine("PART B:");
+            var ordered = int_data.OrderBy(x => x).ToArray();
+            for (int i = 0; i < ordered.Length; ++i)
+            {
+                var current = int_data[i];
+                for (int j = i + 1; j < ordered.Length; ++j)
+                {
+                    var next = int_data[j];
+                    for (int k = j + 1; k < ordered.Length; ++k)
+                    {
+                        var next_next = int_data[k];
+                        if (current + next + next_next == 2020)
+                        {
+                            Console.WriteLine($"1: {current} 2: {next} 3: {next_next}. result: {current * next * next_next}");
+                            break;
+                        }
+                     }
+               }
+            }
+        }    
+    }
+}
