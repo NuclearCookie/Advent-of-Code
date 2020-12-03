4d18fc449cbf410178cbbd13142d11db0c2fccbf
Pieter Vantorre <pietervantorre@gmail.com>
2020-12-03 14:22:30 +0100

    Alternative LINQ implementations and profiling code

--- 2020/C#/Day 1/Program.cs
+++ 2020/C#/Day 1/Program.cs
@@ -1,6 +1,12 @@
-﻿using System.Linq;
+﻿#define PROFILING
+#if !PROFILING
+#define NO_PROFILING
+#endif
+
+using System.Linq;
 using System.IO;
 using System;
+using System.Diagnostics;
 
 namespace Day_1
 {
@@ -10,13 +16,53 @@ namespace Day_1
         {
             var data = File.ReadAllLines("Input/data.txt");
             var int_data = data.Select(x => int.Parse(x)).ToArray();
-            PartA(int_data);
-            PartB(int_data);
+
+            Run("PART A", PartA, int_data);
+            Run("PART A LINQ", PartALINQ, int_data);
+            Run("PART B", PartB, int_data);
+            Run("PART B LINQ", PartBLINQ, int_data);
+        }
+
+        private static void Run(string identifier, Action<int[]> action, int[] int_data)
+        {
+#if PROFILING
+            var watch = Stopwatch.StartNew();
+            for(int i = 0; i < 10000; i++)
+#endif
+                action(int_data);
+#if PROFILING
+            watch.Stop();
+            Console.WriteLine($"{identifier} Time: {watch.ElapsedMilliseconds}");
+#endif
+ }
+
+        private static void PartALINQ(int[] int_data)
+        {
+            WriteLine("PART A (LINQ):");
+
+            var value1 = 0;
+            var value2 = 0;
+            // how to extract Y... 
+            int_data
+                .Where((x, i) =>
+                {
+                    return int_data.Skip(i + 1).Where((y) =>
+                    {
+                        if (x + y == 2020)
+                        {
+                            value1 = x;
+                            value2 = y;
+                            return true;
+                        }
+                        return false;
+                    }).Any();
+                }).Single();
+            WriteLine($"1: {value1} 2: {value2}. result: {value1 * value2}.");
         }
 
         private static void PartA(int[] int_data)
         {
-            Console.WriteLine("PART A:");
+            WriteLine("PART A:");
             for (int i = 0; i < int_data.Length; ++i)
             {
                 var current = int_data[i];
@@ -25,7 +71,7 @@ namespace Day_1
                     var next = int_data[j];
                     if (current + next == 2020)
                     {
-                        Console.WriteLine($"1: {current} 2: {next}. result: {current * next}");
+                        WriteLine($"1: {current} 2: {next}. result: {current * next}.");
                         return;
                     }
                 }
@@ -33,7 +79,7 @@ namespace Day_1
         }
         private static void PartB(int[] int_data)
         {
-            Console.WriteLine("PART B:");
+            WriteLine("PART B:");
             Array.Sort(int_data);
             var ordered = int_data;
             for (int i = 0; i < ordered.Length; ++i)
@@ -47,12 +93,47 @@ namespace Day_1
                         var next_next = int_data[k];
                         if (current + next + next_next == 2020)
                         {
-                            Console.WriteLine($"1: {current} 2: {next} 3: {next_next}. result: {current * next * next_next}");
+                            WriteLine($"1: {current} 2: {next} 3: {next_next}. result: {current * next * next_next}.");
                             return;
                         }
                      }
                }
             }
         }    
+        private static void PartBLINQ(int[] int_data)
+        {
+            WriteLine("PART B (LINQ):");
+
+            var value1 = 0;
+            var value2 = 0;
+            var value3 = 0;
+            var ordered = int_data.OrderBy(x => x);
+            var result = ordered
+                .Where((x, i) =>
+                {
+                    return ordered.Skip(i + 1).Where((y, j) =>
+                    {
+                        return ordered.Skip(j + 1).Where((z) =>
+                        {
+                            if (x + y + z == 2020)
+                            {
+                                value1 = x;
+                                value2 = y;
+                                value3 = z;
+                                return true;
+                            }
+                            return false;
+                        }).Any();
+                    }).Any();
+                }).First();
+            WriteLine($"1: {value1} 2: {value2} 3: {value3}. result: {value1 * value2 * value3}.");
+        }
+
+
+        [Conditional("NO_PROFILING")]
+        private static void WriteLine(string line)
+        {
+            Console.WriteLine(line);
+        }
     }
 }
