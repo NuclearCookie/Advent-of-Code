8b2d581806e43a940cebedd48f43caf082f337db
Pieter Vantorre <pietervantorre@gmail.com>
2020-12-11 12:31:43 +0100

    Day 11 part one works now, although it's not as I'd hoped

--- 2020/C#/Day 11/Program.cs
+++ 2020/C#/Day 11/Program.cs
@@ -19,46 +19,51 @@ namespace Day_11
             var input = File.ReadAllLines("Input/data.txt");
             var row_length = input.Length;
             // All seats are empty at first
-            var data = input.SelectMany(x => x.ToCharArray().Select(x => x == 'L' ? SeatState.Empty : SeatState.Floor)).ToArray();
-            while (PopulateSeats(data, row_length)) 
+            var data = input.Select(x => x.ToCharArray().Select(x => x == 'L' ? SeatState.Empty : SeatState.Floor).ToArray()).ToArray();
+            while (PopulateSeats(data)) 
             {
                 //PrintSeats(data, row_length);
                 //Console.Write("Next..");
                 //var name = Console.Read();
             }
-            var occupied_seats = data.Where(x => x == SeatState.Occupied).Count();
+            var occupied_seats = data.Aggregate(0, (total, x) => total + x.Where(y => y == SeatState.Occupied).Count());
             Console.WriteLine($"Occupied seats after stabilizing: {occupied_seats}");
         }
 
-        private static bool PopulateSeats(SeatState[] data, int row_length)
+        private static bool PopulateSeats(SeatState[][] data)
         {
-            var cloned_data = new SeatState[data.Length];
-            var changed = false;
-            Array.Copy(data, cloned_data, data.Length);
-            for(int index = 0; index < cloned_data.Length; ++index)
+            var cloned_data = new SeatState[data.Length][];
+            for(int row = 0; row < cloned_data.Length; ++row)
             {
-                var occupied_seat = cloned_data[index];
-                if (occupied_seat == SeatState.Floor)
-                    continue;
-                var occupied_neighbours_count = CalculateOccupiedNeighboursCount(cloned_data, index, row_length);
-                if (occupied_seat == SeatState.Occupied && occupied_neighbours_count >= 4)
+                cloned_data[row] = new SeatState[data[row].Length];
+                Array.Copy(data[row], cloned_data[row], data[row].Length);
+            }
+            var changed = false;
+            for(int x = 0; x < cloned_data.Length; ++x)
+            {
+                for (int y = 0; y < cloned_data[x].Length; ++y)
                 {
-                    data[index] = SeatState.Empty;
-                    changed = true;
-                }
-                else if (occupied_seat == SeatState.Empty && occupied_neighbours_count == 0)
-                {
-                    data[index] = SeatState.Occupied;
-                    changed = true;
+                    var occupied_seat = cloned_data[x][y];
+                    if (occupied_seat == SeatState.Floor)
+                        continue;
+                    var occupied_neighbours_count = CalculateOccupiedNeighboursCount(cloned_data, x, y);
+                    if (occupied_seat == SeatState.Occupied && occupied_neighbours_count >= 4)
+                    {
+                        data[x][y] = SeatState.Empty;
+                        changed = true;
+                    }
+                    else if (occupied_seat == SeatState.Empty && occupied_neighbours_count == 0)
+                    {
+                        data[x][y] = SeatState.Occupied;
+                        changed = true;
+                    }
                 }
             }
             return changed;
         }
 
-        private static int CalculateOccupiedNeighboursCount(SeatState[] data, int index, int row_length)
+        private static int CalculateOccupiedNeighboursCount(SeatState[][] data, int self_coord_x, int self_coord_y)
         {
-            var self_coord_x = index % row_length;
-            var self_coord_y = index / row_length;
             var x_start = self_coord_x - 1;
             var x_end = self_coord_x + 1;
             var y_start = self_coord_y - 1;
@@ -66,18 +71,18 @@ namespace Day_11
             var occupied_neighbour_count = 0;
             for(var x = x_start; x <= x_end; ++x)
             {
-                if (x < 0 || x >= row_length)
+                if (x < 0 || x >= data.Length)
                     continue;
                 for(var y = y_start; y <= y_end; ++y)
                 {
-                    if (y < 0 || y >= data.Length / row_length)
+                    if (y < 0 || y >= data[x].Length)
                         continue;
 
                     // skip self
                     if (x == self_coord_x && y == self_coord_y)
                         continue;
 
-                    if (data[x+(row_length * y)] == SeatState.Occupied)
+                    if (data[x][y] == SeatState.Occupied)
                     {
                         occupied_neighbour_count++;
                     }
@@ -86,24 +91,26 @@ namespace Day_11
             return occupied_neighbour_count;
         }
 
-        private static void PrintSeats(SeatState[] data, int row_length)
+        private static void PrintSeats(SeatState[][] data, int row_length)
         {
             var builder = new StringBuilder();
-            for(int i = 0; i < data.Length; ++i)
+            for(int x = 0; x < data.Length; ++x)
             {
-                if (i % row_length == 0)
-                    builder.AppendLine();
-                switch (data[i])
+                builder.AppendLine();
+                for (int y = 0; y < data[x].Length; ++y)
                 {
-                    case SeatState.Empty:
-                        builder.Append('L');
-                        break;
-                    case SeatState.Floor:
-                        builder.Append('.');
-                        break;
-                    case SeatState.Occupied:
-                        builder.Append('#');
-                        break;
+                    switch (data[x][y])
+                    {
+                        case SeatState.Empty:
+                            builder.Append('L');
+                            break;
+                        case SeatState.Floor:
+                            builder.Append('.');
+                            break;
+                        case SeatState.Occupied:
+                            builder.Append('#');
+                            break;
+                    }
                 }
             }
             Console.WriteLine(builder.ToString());
