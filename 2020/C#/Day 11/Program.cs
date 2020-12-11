fba5721a68a7ed5f7ec606a816a67d2a1362a591
Pieter Vantorre <pietervantorre@gmail.com>
2020-12-11 15:08:54 +0100

    part 2 + timing done. Pretty slow but it works

--- 2020/C#/Day 11/Program.cs
+++ 2020/C#/Day 11/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -14,15 +16,50 @@ namespace Day_11
             Empty,
             Occupied
         }
+
+        private enum Direction : short
+        {
+            NorthWest,
+            North,
+            NorthEast,
+            East,
+            SouthEast,
+            South,
+            SouthWest,
+            West
+        };
+
+        private struct Coord
+        {
+            public int X;
+            public int Y;
+        };
+
         static void Main(string[] args)
         {
             var input = File.ReadAllLines("Input/data.txt");
             var row_length = input.Length;
             // All seats are empty at first
             var data = input.Select(x => x.ToCharArray().Select(x => x == 'L' ? SeatState.Empty : SeatState.Floor).ToArray()).ToArray();
-            while (PopulateSeats(data)) 
+            var data_a = CloneData(data);
+            var data_b = CloneData(data);
+            var timer = Stopwatch.StartNew();
+            PartA(data_a);
+            timer.Stop();
+            Console.WriteLine($"Time Part A: {timer.ElapsedMilliseconds} ms");
+            timer.Reset();
+            timer.Start();
+            PartB(data_b);
+            timer.Stop();
+            Console.WriteLine($"Time Part A: {timer.ElapsedMilliseconds} ms");
+        }
+
+        private static void PartA(SeatState[][] data)
+        {
+            Console.WriteLine("Part A:");
+            while (PopulateSeats(data, max_occupied_seats: 4, count_floors: true))
             {
-                //PrintSeats(data, row_length);
+                //PrintSeats(data);
                 //Console.Write("Next..");
                 //var name = Console.Read();
             }
@@ -30,24 +67,32 @@ namespace Day_11
             Console.WriteLine($"Occupied seats after stabilizing: {occupied_seats}");
         }
 
-        private static bool PopulateSeats(SeatState[][] data)
+        private static void PartB(SeatState[][] data)
         {
-            var cloned_data = new SeatState[data.Length][];
-            for(int row = 0; row < cloned_data.Length; ++row)
+            Console.WriteLine("Part B:");
+            while (PopulateSeats(data, max_occupied_seats: 5, count_floors: false))
             {
-                cloned_data[row] = new SeatState[data[row].Length];
-                Array.Copy(data[row], cloned_data[row], data[row].Length);
+                //PrintSeats(data);
+                //Console.Write("Next..");
+                //var name = Console.Read();
             }
+            var occupied_seats = data.Aggregate(0, (total, x) => total + x.Where(y => y == SeatState.Occupied).Count());
+            Console.WriteLine($"Occupied seats after stabilizing: {occupied_seats}");
+
+        }
+        private static bool PopulateSeats(SeatState[][] data, int max_occupied_seats, bool count_floors)
+        {
+            SeatState[][] cloned_data = CloneData(data);
             var changed = false;
-            for(int x = 0; x < cloned_data.Length; ++x)
+            for (int x = 0; x < cloned_data.Length; ++x)
             {
                 for (int y = 0; y < cloned_data[x].Length; ++y)
                 {
                     var occupied_seat = cloned_data[x][y];
                     if (occupied_seat == SeatState.Floor)
                         continue;
-                    var occupied_neighbours_count = CalculateOccupiedNeighboursCount(cloned_data, x, y);
-                    if (occupied_seat == SeatState.Occupied && occupied_neighbours_count >= 4)
+                    var occupied_neighbours_count = CalculateOccupiedNeighboursCount(cloned_data, new Coord { X = x, Y = y }, count_floors);
+                    if (occupied_seat == SeatState.Occupied && occupied_neighbours_count >= max_occupied_seats)
                     {
                         data[x][y] = SeatState.Empty;
                         changed = true;
@@ -62,36 +107,76 @@ namespace Day_11
             return changed;
         }
 
-        private static int CalculateOccupiedNeighboursCount(SeatState[][] data, int self_coord_x, int self_coord_y)
+        private static SeatState[][] CloneData(SeatState[][] data)
         {
-            var x_start = self_coord_x - 1;
-            var x_end = self_coord_x + 1;
-            var y_start = self_coord_y - 1;
-            var y_end = self_coord_y + 1;
-            var occupied_neighbour_count = 0;
-            for(var x = x_start; x <= x_end; ++x)
+            var cloned_data = new SeatState[data.Length][];
+            for (int row = 0; row < cloned_data.Length; ++row)
             {
-                if (x < 0 || x >= data.Length)
-                    continue;
-                for(var y = y_start; y <= y_end; ++y)
-                {
-                    if (y < 0 || y >= data[x].Length)
-                        continue;
-
-                    // skip self
-                    if (x == self_coord_x && y == self_coord_y)
-                        continue;
-
-                    if (data[x][y] == SeatState.Occupied)
-                    {
-                        occupied_neighbour_count++;
-                    }
-                }
+                cloned_data[row] = new SeatState[data[row].Length];
+                Array.Copy(data[row], cloned_data[row], data[row].Length);
             }
-            return occupied_neighbour_count;
+
+            return cloned_data;
         }
 
-        private static void PrintSeats(SeatState[][] data, int row_length)
+        private static int CalculateOccupiedNeighboursCount(SeatState[][] data, Coord self_coord, bool count_floors)
+        {
+            var occupied_neighbours = Enumerable.Range((int)Direction.NorthWest, (int)Direction.West + 1)
+                .Select(dir =>
+                {
+                    return GetFirstSeatStateInDirection(data, self_coord, (Direction)dir, count_floors);
+                }).Where(neighbour_state => neighbour_state is SeatState.Occupied).Count();
+            return occupied_neighbours;
+        }
+
+        private static IEnumerable<SeatState> GetNextSeatInDirection(SeatState[][] data, Coord self_coord, Direction dir)
+        {
+            switch (dir)
+            {
+                case Direction.West:
+                    for (int y = self_coord.Y - 1; y >= 0; --y)
+                        yield return data[self_coord.X][y];
+                    yield break;
+                case Direction.East:
+                    for (int y = self_coord.Y + 1; y < data[self_coord.X].Length; ++y)
+                        yield return data[self_coord.X][y];
+                    yield break;
+                case Direction.South:
+                    for (int x = self_coord.X + 1; x < data.Length; ++x)
+                        yield return data[x][self_coord.Y];
+                    yield break;
+                case Direction.North:
+                    for (int x = self_coord.X - 1; x >= 0; --x)
+                        yield return data[x][self_coord.Y];
+                    yield break;
+                case Direction.SouthWest:
+                    for (int y = self_coord.Y - 1, x = self_coord.X + 1; y >= 0 && x < data.Length; --y, ++x)
+                        yield return data[x][y];
+                    yield break;
+                case Direction.NorthWest:
+                    for (int y = self_coord.Y - 1, x = self_coord.X - 1; y >= 0 && x >= 0; --y, --x)
+                        yield return data[x][y];
+                    yield break;
+                case Direction.NorthEast:
+                    for (int y = self_coord.Y + 1, x = self_coord.X -1; y < data[self_coord.X].Length && x >= 0; ++y, --x)
+                        yield return data[x][y];
+                    yield break;
+                case Direction.SouthEast:
+                    for (int y = self_coord.Y + 1, x = self_coord.X + 1; y < data[self_coord.X].Length && x < data.Length; ++y, ++x)
+                        yield return data[x][y];
+                    yield break;
+            }
+        }
+
+        private static SeatState GetFirstSeatStateInDirection(SeatState[][] data, Coord self_coord, Direction dir, bool count_floor)
+        {
+            foreach (var state in GetNextSeatInDirection(data, self_coord, dir))
+                if (count_floor || state != SeatState.Floor)
+                    return state;
+            return SeatState.Floor;
+        }
+
+        private static void PrintSeats(SeatState[][] data)
         {
             var builder = new StringBuilder();
             for(int x = 0; x < data.Length; ++x)
