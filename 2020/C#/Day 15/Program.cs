ecfb5a326b09d05fd572c861c68e7cb9569b7a44
Pieter Vantorre <pietervantorre@gmail.com>
2020-12-15 18:08:52 +0100

    IDictionary and ints

--- 2020/C#/Day 15/Program.cs
+++ 2020/C#/Day 15/Program.cs
@@ -27,9 +27,9 @@ namespace Day_15
         {
             Console.WriteLine("Part A:");
             var start_input = input.Split(',');
-            var numbers_map = start_input.Select((x, i) => (number: ulong.Parse(x), index: (ulong)i)).ToDictionary(x => x.number, x => x.index);
-            var start_index = (ulong)start_input.Length;
-            var last_spoken_number = Play(numbers_map, start_index, 2020uL);
+            var numbers_map = start_input.Select((x, i) => (number: int.Parse(x), index: i)).ToDictionary(x => x.number, x => x.index);
+            var start_index = start_input.Length;
+            var last_spoken_number = Play(numbers_map, start_index, 2020);
             Console.WriteLine($"The 2020th spoken number is {last_spoken_number}");
         }
 
@@ -37,18 +37,18 @@ namespace Day_15
         {
             Console.WriteLine("Part B:");
             var start_input = input.Split(',');
-            var numbers_map = start_input.Select((x, i) => (number: ulong.Parse(x), index: (ulong)i)).ToDictionary(x => x.number, x => x.index);
-            var start_index = (ulong)start_input.Length;
-            var last_spoken_number = Play(numbers_map, start_index, 30000000uL);
+            var numbers_map = start_input.Select((x, i) => (number: int.Parse(x), index: i)).ToDictionary(x => x.number, x => x.index);
+            var start_index = start_input.Length;
+            var last_spoken_number = Play(numbers_map, start_index, 30000000);
             Console.WriteLine($"The 30000000th spoken number is {last_spoken_number}");
         }
-        private static ulong Play(Dictionary<ulong, ulong> numbers_map, ulong start_index, ulong end_index)
+        private static int Play(IDictionary<int, int> numbers_map, int start_index, int end_index)
         {
-            var last_spoken_number = 0uL;
+            var last_spoken_number = 0;
             end_index -= 1;
-            for (ulong i = start_index; i < end_index; ++i)
+            for (int i = start_index; i < end_index; ++i)
             {
-                ulong new_number = 0;
+                int new_number = 0;
                 if (numbers_map.TryGetValue(last_spoken_number, out var previous_index))
                     new_number = i - previous_index;
                 numbers_map[last_spoken_number] = i;
