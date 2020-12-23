a429337c6b2f66ab2176cccb2486887d196b9dc7
Pieter Vantorre <pietervantorre@gmail.com>
2020-12-23 19:02:20 +0100

    Change to list based approach

--- 2020/C#/Day 23/Program.cs
+++ 2020/C#/Day 23/Program.cs
@@ -10,8 +10,8 @@ namespace Day_23
     {
         static void Main(string[] args)
         {
-            PartA();
-            //PartB();
+            //PartA();
+            PartB();
         }
 
         private static void PartA()
@@ -19,16 +19,12 @@ namespace Day_23
             Console.WriteLine("Part A:");
             var data = File.ReadAllText("Input/data.txt").Select(x => (int)char.GetNumericValue(x)).ToList();
             var max_cup = data.Count;
-            var linked_list = new LinkedList<int>(data);
-            Play(linked_list, rounds: 100);
-            var label_node = linked_list.Find(1);
+            Play(data, rounds: 100);
+            var index_of_cup_1 = data.IndexOf(1);
             var result = new StringBuilder();
             for (var i = 1; i < data.Count; ++i)
             {
-                label_node = label_node.Next;
-                if (label_node == null)
-                    label_node = linked_list.First;
-                result.Append(label_node.Value);
+                result.Append(data[(i + index_of_cup_1) % data.Count]);
             }
             Console.WriteLine($"Game result: {result.ToString()}");
         }
@@ -38,59 +34,53 @@ namespace Day_23
             Console.WriteLine("Part B:");
             var data = File.ReadAllText("Input/test.txt").Select(x => (int)char.GetNumericValue(x));
             var max_cup = data.Count();
-            data = data.Concat(Enumerable.Range(max_cup + 1, 1000000 - max_cup));
-            var linked_list = new LinkedList<int>(data);
-            Play(linked_list, rounds: 10000000);
-            var node_label = linked_list.Find(1);
+            var list = data.Concat(Enumerable.Range(max_cup + 1, 1000000 - max_cup + 1)).ToList();
+            Play(list, rounds: 10000000);
+            var index_of_cup_1 = list.IndexOf(1);
             var result = 1uL;
-            for (var i = 0; i < 2; ++i)
+            for (var i = 1; i < 3; ++i)
             {
-                node_label = node_label.Next;
-                result *= (ulong)node_label.Value;
+                result *= (ulong)list[(i + index_of_cup_1) % list.Count];
             }
             Console.WriteLine($"Game result: {result}");
         }
 
-        private static void Play(LinkedList<int> data, int rounds)
+        private static void Play(List<int> data, int rounds)
         {
             var max_cup = data.Count;
-            var current_cup_node = data.First;
-            var picked_up_cups = new LinkedListNode<int>[3];
-            var picked_up_cup_values = new int[3];
+            var current_cup_index = 0;
+            var picked_up_cups = new int[3];
+            var picked_up_cup_indices = new int[3];
             for (int round = 0; round < rounds; ++round)
             {
-                var next = current_cup_node.Next;
+                var current_cup = data[current_cup_index];
                 for (int i = 0; i < 3; ++i)
                 {
-                    if (next == null)
-                        next = data.First;
-                    picked_up_cups[i] = next;
-                    picked_up_cup_values[i] = next.Value;
-                    var to_remove = next;
-                    next = next.Next;
-                    data.Remove(to_remove);
+                    var corrected_index = (current_cup_index + 1 + i) % data.Count;
+                    picked_up_cups[i] = data[corrected_index];
+                    picked_up_cup_indices[i] = corrected_index;
+                }
+                for (int i = 0; i < 3; ++i)
+                {
+                    data.RemoveAt(picked_up_cup_indices[i]);
                 }
 
-                LinkedListNode<int> next_cup_node = null;
-                var next_cup = current_cup_node.Value;
-                while (next_cup_node == null)
+                var next_cup_index = -1;
+                var next_cup = current_cup;
+                while (next_cup_index == -1)
                 {
                     next_cup--;
                     if (next_cup < 1)
                     {
                         next_cup = max_cup;
                     }
-                    if (Array.IndexOf(picked_up_cup_values, next_cup) == -1)
-                        next_cup_node = data.Find(next_cup);
+                    if (Array.IndexOf(picked_up_cups, next_cup) == -1)
+                        next_cup_index = data.IndexOf(next_cup);
                 }
-                for(int i = 0; i < 3; ++i)
-                {
-                    data.AddAfter(next_cup_node, picked_up_cups[i]);
-                    next_cup_node = next_cup_node.Next;
-                }
-                current_cup_node = current_cup_node.Next;
-                if (current_cup_node == null)
-                    current_cup_node = data.First;
+                data.InsertRange(next_cup_index + 1, picked_up_cups);
+                current_cup_index = data.IndexOf(current_cup) + 1;
+                if (current_cup_index >= data.Count)
+                    current_cup_index = 0;
             }
         }
     }
