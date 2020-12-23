668c33bed402788725342b64fa10f9fa55c1f4a2
Pieter Vantorre <pietervantorre@gmail.com>
2020-12-23 21:33:48 +0100

    Day 23 part B.
    Cheated to find it, although I was close.
    I had the idea to create an index buffer where
    arr[label] == index.
    The proper solution was arr[label] == label + 1 (clockwise)

--- 2020/C#/Day 23/Program.cs
+++ 2020/C#/Day 23/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -10,8 +11,16 @@ namespace Day_23
     {
         static void Main(string[] args)
         {
-            //PartA();
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            PartA();
+            stopwatch.Stop();
+            Console.WriteLine($"duration: {stopwatch.ElapsedMilliseconds}ms");
+            stopwatch.Reset();
+            stopwatch.Start();
             PartB();
+            stopwatch.Stop();
+            Console.WriteLine($"duration: {stopwatch.ElapsedMilliseconds}ms");
         }
 
         private static void PartA()
@@ -19,55 +28,74 @@ namespace Day_23
             Console.WriteLine("Part A:");
             var data = File.ReadAllText("Input/data.txt").Select(x => (int)char.GetNumericValue(x)).ToList();
             var max_cup = data.Count;
-            Play(data, rounds: 100);
-            var index_of_cup_1 = data.IndexOf(1);
-            var result = new StringBuilder();
-            for (var i = 1; i < data.Count; ++i)
+            Console.WriteLine("Start converting data.");
+            var converted_data = new List<int>(data.Count);
+            for (int i= 0; i < data.Count; ++i)
             {
-                result.Append(data[(i + index_of_cup_1) % data.Count]);
+                converted_data.Add(data[(data.IndexOf(i + 1) + 1) % data.Count]);
             }
-            Console.WriteLine($"Game result: {result.ToString()}");
+            Console.WriteLine("Done converting data.");
+
+            var indexed_data = Play(converted_data, data[0], rounds: 100);
+            var next_label = indexed_data[1 - 1];
+            var result = new StringBuilder();
+            for (var i = 1; i < indexed_data.Count; ++i)
+            {
+                result.Append(next_label);
+                next_label = indexed_data[next_label - 1];
+            }
+            Console.WriteLine($"Game result: {result}");
         }
 
         private static void PartB()
         {
             Console.WriteLine("Part B:");
-            var data = File.ReadAllText("Input/test.txt").Select(x => (int)char.GetNumericValue(x));
-            var max_cup = data.Count();
-            var list = data.Concat(Enumerable.Range(max_cup + 1, 1000000 - max_cup + 1)).ToList();
-            Play(list, rounds: 10000000);
-            var index_of_cup_1 = list.IndexOf(1);
+            var data = File.ReadAllText("Input/data.txt").Select(x => (int)char.GetNumericValue(x)).ToList();
+            var max_cup = data.Count;
+            Console.WriteLine("Start converting data.");
+            var converted_data = new List<int>(10000000);
+            for (int i= 0; i < data.Count; ++i)
+            {
+                converted_data.Add(data[(data.IndexOf(i + 1) + 1) % data.Count]);
+            }
+            Console.WriteLine("Done converting data.");
+
+            var extended_converted_data = converted_data.Concat(Enumerable.Range(max_cup + 2, 1000000 - max_cup)).ToList();
+            var warp_around_label = data[data.Count - 1];
+            var first_label = data[0];
+            extended_converted_data[warp_around_label - 1] = data.Count + 1;
+            extended_converted_data[extended_converted_data.Count - 1] = first_label;
+            var indexed_data = Play(extended_converted_data, first_label, rounds: 10000000);
+            var next_label = indexed_data[1 - 1];
             var result = 1uL;
             for (var i = 1; i < 3; ++i)
             {
-                result *= (ulong)list[(i + index_of_cup_1) % list.Count];
+                result *= (ulong)next_label;
+                next_label = indexed_data[next_label - 1];
             }
             Console.WriteLine($"Game result: {result}");
         }
 
-        private static void Play(List<int> data, int rounds)
+        private static List<int> Play(List<int> converted_data, int start_index, int rounds)
         {
-            var max_cup = data.Count;
-            var current_cup_index = 0;
+            // convert the data so that arr[label] == clockwise of label
+            var max_cup = converted_data.Count;
+            var current_cup_index = start_index;
             var picked_up_cups = new int[3];
-            var picked_up_cup_indices = new int[3];
             for (int round = 0; round < rounds; ++round)
             {
-                var current_cup = data[current_cup_index];
+                var current_cup = current_cup_index;
+                var next = current_cup_index;
                 for (int i = 0; i < 3; ++i)
                 {
-                    var corrected_index = (current_cup_index + 1 + i) % data.Count;
-                    picked_up_cups[i] = data[corrected_index];
-                    picked_up_cup_indices[i] = corrected_index;
+                    next = converted_data[next - 1];
+                    picked_up_cups[i] = next;
                 }
-                for (int i = 0; i < 3; ++i)
-                {
-                    data.RemoveAt(picked_up_cup_indices[i]);
-                }
+                // eliminate all 3 picked up cups.
+                converted_data[current_cup_index - 1] = converted_data[next-1];
 
-                var next_cup_index = -1;
                 var next_cup = current_cup;
-                while (next_cup_index == -1)
+                while (true)
                 {
                     next_cup--;
                     if (next_cup < 1)
@@ -75,13 +103,14 @@ namespace Day_23
                         next_cup = max_cup;
                     }
                     if (Array.IndexOf(picked_up_cups, next_cup) == -1)
-                        next_cup_index = data.IndexOf(next_cup);
+                        break;
                 }
-                data.InsertRange(next_cup_index + 1, picked_up_cups);
-                current_cup_index = data.IndexOf(current_cup) + 1;
-                if (current_cup_index >= data.Count)
-                    current_cup_index = 0;
+                var after_reinsert = converted_data[next_cup - 1];
+                converted_data[next_cup - 1] = picked_up_cups[0];
+                converted_data[picked_up_cups[2] - 1] = after_reinsert;
+                current_cup_index = converted_data[current_cup_index - 1];
             }
+            return converted_data;
         }
     }
 }
