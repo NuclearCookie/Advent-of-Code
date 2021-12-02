d1a961de587f026c83282a3a81c64bc7fd3bef7e
Pieter Vantorre <pietervantorre@gmail.com>
2021-12-02 19:54:23 +0100

    Day 01

---
+++ 2021/C#/Day01/Day01.cs
@@ -0,0 +1,25 @@
+﻿using Library;
+
+var depths = IO.ReadInputAsIntArray().ToArray();
+var totalIncreases = 0;
+Console.WriteLine("Part one");
+for(int i = 1; i < depths.Length; ++i)
+{
+    if (depths[i - 1] < depths[i])
+    {
+        totalIncreases++;
+    }
+}
+Console.WriteLine($"Total increases: {totalIncreases}");
+Console.WriteLine("Part two");
+totalIncreases = 0;
+for(int i = 3; i < depths.Length; ++i)
+{
+    var prevSum = depths[i - 3] + depths[i - 2] + depths[i - 1];
+    var currSum = depths[i - 2] + depths[i - 1] + depths[i];
+    if (prevSum < currSum)
+    {
+        totalIncreases++;
+    }
+}
+Console.WriteLine($"Total increases: {totalIncreases}");
