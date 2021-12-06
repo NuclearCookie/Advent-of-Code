5f4ac4be737b4ebf49f6de0723fcd6f705b20330
Pieter Vantorre <pietervantorre@gmail.com>
2021-12-06 20:41:35 +0100

    Day 06

--- 2021/C#/Day06/Day06.cs
+++ 2021/C#/Day06/Day06.cs
@@ -1,8 +1,21 @@
 ﻿using Library;
 
-var fish = IO.ReadInputAsStringArray().First().Split(',').Select(int.Parse).ToList();
+var fish = IO.ReadInputAsStringArray().First().Split(',').Select(long.Parse);
+long fish0 = fish.Where(x => x == 0).Count();
+long fish1 = fish.Where(x => x == 1).Count();
+long fish2 = fish.Where(x => x == 2).Count();
+long fish3 = fish.Where(x => x == 3).Count();
+long fish4 = fish.Where(x => x == 4).Count();
+long fish5 = fish.Where(x => x == 5).Count();
+long fish6 = fish.Where(x => x == 6).Count();
+long fish7 = fish.Where(x => x == 7).Count();
+long fish8 = fish.Where(x => x == 8).Count();
 
+var watch = System.Diagnostics.Stopwatch.StartNew();
 PartA();
+PartB();
+watch.Stop();
+Console.WriteLine($"Time: {watch.ElapsedMilliseconds}ms");
 
 void PartA()
 {
@@ -10,22 +23,29 @@ void PartA()
     {
         ProcessDay();
     }
-    Console.WriteLine($"After 80 days, {fish.Count} lanternfishes appeared.");
+    Console.WriteLine($"After 80 days, {fish0 + fish1 + fish2 + fish3 + fish4 + fish5 + fish6 + fish7 + fish8} lanternfishes appeared.");
 }
+
+void PartB()
+{
+    for(int i = 80; i < 256; ++i)
+    {
+        ProcessDay();
+    }
+    Console.WriteLine($"After 256 days, {fish0 + fish1 + fish2 + fish3 + fish4 + fish5 + fish6 + fish7 + fish8} lanternfishes appeared.");
+}
+
 void ProcessDay()
 {
-    int fishesToAppend = 0;
-    for (int i = 0; i < fish.Count; i++)
-    {
-        int fishy = fish[i];
-        fishy--;
-        if (fishy < 0)
-        {
-            fishesToAppend++;
-            fishy = 6;
-        }
-        fish[i] = fishy;
-    }
-    fish.AddRange(Enumerable.Repeat(8, fishesToAppend));
+    var temp = fish0; // this will go to fish9
+    fish0 = fish1;
+    fish1 = fish2;
+    fish2 = fish3;
+    fish3 = fish4;
+    fish4 = fish5;
+    fish5 = fish6;
+    fish6 = fish7 + temp;
+    fish7 = fish8;
+    fish8 = temp;
 }
 
