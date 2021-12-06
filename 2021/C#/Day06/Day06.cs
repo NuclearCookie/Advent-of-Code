2d277c60a5bdd4ea7de0335d6f5792cd2f37ab6b
Pieter Vantorre <pietervantorre@gmail.com>
2021-12-06 20:06:44 +0100

    Day 06 part 1

--- 2021/C#/Day06/Day06.cs
+++ 2021/C#/Day06/Day06.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Hello World");
+﻿using Library;
+
+var fish = IO.ReadInputAsStringArray().First().Split(',').Select(int.Parse).ToList();
+
+PartA();
+
+void PartA()
+{
+    for(int i = 0; i < 80; ++i)
+    {
+        ProcessDay();
+    }
+    Console.WriteLine($"After 80 days, {fish.Count} lanternfishes appeared.");
+}
+void ProcessDay()
+{
+    int fishesToAppend = 0;
+    for (int i = 0; i < fish.Count; i++)
+    {
+        int fishy = fish[i];
+        fishy--;
+        if (fishy < 0)
+        {
+            fishesToAppend++;
+            fishy = 6;
+        }
+        fish[i] = fishy;
+    }
+    fish.AddRange(Enumerable.Repeat(8, fishesToAppend));
+}
+
