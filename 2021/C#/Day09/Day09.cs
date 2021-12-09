feb4ca459919128d257db94eb11d01697caf537e
Pieter Vantorre <pietervantorre@gmail.com>
2021-12-09 23:49:12 +0100

    Day 09

--- 2021/C#/Day09/Day09.cs
+++ 2021/C#/Day09/Day09.cs
@@ -1,1 +1,71 @@
-﻿Console.WriteLine("Hello World");
+﻿using Library;
+using Library.Datastructures;
+
+var heightField = IO.ReadInputAsStringArray().Select(x => x.ToCharArray().Select(x => x - '0').ToArray()).ToArray();
+Array2D<int> heightField2D = new Array2D<int>(heightField);
+Array2D<bool> processedPoints = new Array2D<bool>(heightField2D.RowLength, heightField2D.ColumnLength);
+
+int[] neighbours = new int[4];
+List<int> lowPointIndices = new List<int>();
+
+PartA();
+PartB();
+
+void PartA()
+{
+    var riskLevel = 0;
+    for (int i = 0; i < heightField2D.Length; ++i)
+    {
+        var height = heightField2D[i];
+        var validNeighbours = heightField2D.GetNeighbours(i, neighbours);
+        if (neighbours[0..validNeighbours].Min() <= height)
+        {
+            continue;
+        }
+
+        lowPointIndices.Add(i);
+        processedPoints[i] = true;
+        riskLevel += 1 + height;
+    }
+    Console.WriteLine($"RiskLevel: {riskLevel}");
+}
+
+void PartB()
+{
+    for(int i = 0; i < heightField2D.Length; ++i)
+    {
+        if (heightField2D[i] == 9)
+        {
+            processedPoints[i] = true;
+        }
+    }
+    List<int> slopeSizes = new List<int>();
+    foreach(var lowPointIndex in lowPointIndices)
+    {
+        slopeSizes.Add(RecursiveFindSlopeSize(lowPointIndex));
+    }
+    slopeSizes.Sort((a, b) => b - a);
+    Console.WriteLine($"Size of 3 biggest basins: {slopeSizes[0]}, {slopeSizes[1]}, {slopeSizes[2]}. Multiplied: {slopeSizes.GetRange(0, 3).Aggregate(1, (curr, val) => curr * val)}");
+}
+
+int RecursiveFindSlopeSize(int currentLowIndex)
+{
+    int result = 1;
+    var currentLow = heightField2D[currentLowIndex];
+    var localNeighbourIndices = new int[4];
+    int neighbourCount = heightField2D.GetNeighbourIndices(currentLowIndex, localNeighbourIndices);
+    for(int i = 0; i < neighbourCount; ++i)
+    {
+        var neighbourIndex = localNeighbourIndices[i];
+        if (processedPoints[neighbourIndex] == false)
+        {
+            var neighbourValue = heightField2D[neighbourIndex];
+            if (neighbourValue > currentLow)
+            {
+                processedPoints[neighbourIndex] = true;
+                result += RecursiveFindSlopeSize(neighbourIndex);
+            }
+        }
+    }
+    return result;
+}
--- 2021/C#/Library/DataStructures.cs
+++ 2021/C#/Library/DataStructures.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -134,6 +136,186 @@ namespace Library.Datastructures
         public bool Contains(int value)
         {
             return value >= Min && value <= Max;
+        }
+    }
+
+    // :TODO: implement IEnumerable etc?
+    public class Array2D<T>
+    {
+        private T[] _internalArray;
+        private int _rows;
+        private int _cols;
+
+        public T this[int i]
+        {
+            get => _internalArray[i];
+            set => _internalArray[i] = value;
+        }
+        public T this[int row, int col]
+        {
+            get => GetElementAt(row, col);
+            set => _internalArray[RowColumnToIndex(row, col)] = value;
+        }
+
+        public int Length => _rows * _cols;
+        public int RowLength => _rows;
+        public int ColumnLength => _cols;
+
+        public Array2D(int rows, int cols)
+        {
+            _internalArray = new T[rows * cols];
+            _rows = rows;
+            _cols = cols;
+        }
+
+        public Array2D(int rows, int cols, T[] initialData) : this(rows, cols)
+        {
+            Debug.Assert(initialData.Length == rows * cols);
+            Array.Copy(initialData, _internalArray, initialData.Length);
+        }
+
+        public Array2D(T[][] initialData)
+        {
+            _rows = initialData.Length;
+            _cols = initialData[0].Length;
+            _internalArray = new T[_rows * _cols];
+            for (int i = 0; i < _rows; ++i)
+            {
+                Array.Copy(initialData[i], 0, _internalArray, i * _cols, _cols);
+            }
+        }
+
+        public void GetRow(int row, T[] outputBuffer)
+        {
+            Debug.Assert(row >= 0 && row < _rows && outputBuffer.Length == _cols);
+            Array.Copy(_internalArray, row * _cols, outputBuffer, 0, _cols);
+        }
+
+        public void GetColumn(int column, T[] outputBuffer)
+        {
+            Debug.Assert(column >= 0 && column < _cols && outputBuffer.Length == _rows);
+            for(int i = 0; i < outputBuffer.Length; ++i)
+            {
+                outputBuffer[i] = _internalArray[RowColumnToIndex(i, column)];
+            }
+        }
+
+        public T GetElementAt(int row, int col)
+        {
+            return _internalArray[RowColumnToIndex(row, col)];
+        }
+        public void IndexToRowColumn(int index, out int row, out int col)
+        {
+            row = index / _cols;
+            col = index % _cols;
+        }
+
+        public int RowColumnToIndex(int row, int col)
+        {
+            Debug.Assert(row < _rows && col < _cols);
+            return col + (row * _cols);
+        }
+
+        public int GetNeighbourIndices(int index, int[] outputBuffer)
+        {
+            IndexToRowColumn(index, out var row, out var col);
+            return GetNeighbourIndices(row, col, outputBuffer);
+        }
+
+        public int GetNeighbourIndices(int row, int col, int[] outputBuffer)
+        {
+            var index = 0;
+            // only direct neighbours
+            if (outputBuffer.Length >= 4)
+            {
+                if (row > 0)
+                {
+                    outputBuffer[index++] = RowColumnToIndex(row - 1, col);
+                }
+                if (row < _rows - 1)
+                {
+                    outputBuffer[index++] = RowColumnToIndex(row + 1, col);
+                }
+                if (col > 0)
+                {
+                    outputBuffer[index++] = RowColumnToIndex(row, col - 1);
+                }
+                if (col < _cols - 1)
+                {
+                    outputBuffer[index++] = RowColumnToIndex(row, col + 1);
+                }
+            }
+            if (outputBuffer.Length >= 8) // also diagonal
+            {
+                if (row > 0 && col > 0)
+                {
+                    outputBuffer[index++] = RowColumnToIndex(row - 1, col - 1);
+                }
+                if (row < _rows - 1 && col < _cols - 1)
+                {
+                    outputBuffer[index++] = RowColumnToIndex(row + 1, col + 1);
+                }
+                if (row < _rows - 1 && col > 0)
+                {
+                    outputBuffer[index++] = RowColumnToIndex(row + 1, col - 1);
+                }
+                if (row < _rows - 1 && col < _cols - 1)
+                {
+                    outputBuffer[index++] = RowColumnToIndex(row -1, col + 1);
+                }
+            }
+            return index;
+        }
+
+        public int GetNeighbours(int index, T[] outputBuffer)
+        {
+            IndexToRowColumn(index, out int row, out int col);
+            return GetNeighbours(row, col, outputBuffer);
+        }
+
+        public int GetNeighbours(int row, int col, T[] outputBuffer)
+        {
+            var index = 0;
+            // only direct neighbours
+            if (outputBuffer.Length >= 4)
+            {
+                if (row > 0)
+                {
+                    outputBuffer[index++] = GetElementAt(row - 1, col);
+                }
+                if (row < _rows - 1)
+                {
+                    outputBuffer[index++] = GetElementAt(row + 1, col);
+                }
+                if (col > 0)
+                {
+                    outputBuffer[index++] = GetElementAt(row, col - 1);
+                }
+                if (col < _cols - 1)
+                {
+                    outputBuffer[index++] = GetElementAt(row, col + 1);
+                }
+            }
+            if (outputBuffer.Length >= 8) // also diagonal
+            {
+                if (row > 0 && col > 0)
+                {
+                    outputBuffer[index++] = GetElementAt(row - 1, col - 1);
+                }
+                if (row < _rows - 1 && col < _cols - 1)
+                {
+                    outputBuffer[index++] = GetElementAt(row + 1, col + 1);
+                }
+                if (row < _rows - 1 && col > 0)
+                {
+                    outputBuffer[index++] = GetElementAt(row + 1, col - 1);
+                }
+                if (row < _rows - 1 && col < _cols - 1)
+                {
+                    outputBuffer[index++] = GetElementAt(row -1, col + 1);
+                }
+            }
+            return index;
         }
     }
 }
