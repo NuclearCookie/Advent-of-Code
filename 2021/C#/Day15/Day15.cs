b7e5a6e3dc5c267bcecb1c5abf58ea90c9653109
Pieter Vantorre <pietervantorre@gmail.com>
2021-12-16 00:20:49 +0100

    Speed up the algorithm and implemented part B

--- 2021/C#/Day15/Day15.cs
+++ 2021/C#/Day15/Day15.cs
@@ -4,16 +4,55 @@ using Library.Algorithms;
 
 Array2D<int> grid = new Array2D<int>(IO.ReadInputAsStringArray().Select(x => x.Select(x => x - '0').ToArray()).ToArray());
 List<Point2> outPath = new List<Point2>();
-//grid.Transpose();
-Algorithms.AStarPath(grid, new Point2(0, 0), new Point2(grid.ColumnLength - 1, grid.RowLength - 1), outPath);
 
 PartA();
+PartB();
 
 void PartA()
 {
+    Algorithms.AStarPath(grid, new Point2(0, 0), new Point2(grid.ColumnLength - 1, grid.RowLength - 1), outPath);
     var totalRisk = outPath.Skip(1).Select(coord => grid[coord]).Sum();
-    Console.WriteLine($"Total risk: {totalRisk}");
-    for(int row = 0; row < grid.RowLength; ++row)
+    Console.WriteLine($"Part A: Total risk: {totalRisk}");
+}
+
+void PartB()
+{
+    Array2D<int> extendedGrid = new Array2D<int>(grid.RowLength * 5, grid.ColumnLength * 5);
+    for(int row = 0; row < grid.ColumnLength; ++row)
+    {
+        int[] extendedRow = new int[extendedGrid.ColumnLength];
+        for (int col = 0; col < grid.RowLength; ++col)
+        {
+            var currentValue = grid[row, col];
+            for (int i = 0; i < 5; ++i)
+            {
+                extendedRow[col + i * grid.ColumnLength] = currentValue;
+                currentValue = (currentValue % 9) + 1;
+            }
+        }
+
+        for (int i = 0; i < 5; ++i)
+        {
+            extendedGrid.SetRow(row + i * grid.RowLength, extendedRow);
+            for(int col = 0; col < extendedRow.Length; ++col)
+            {
+                var currentValue = extendedRow[col];
+                currentValue = (currentValue % 9) + 1;
+                extendedRow[col] = currentValue;
+            }
+        }
+    }
+    outPath.Clear();
+    Algorithms.AStarPath(extendedGrid, new Point2(0, 0), new Point2(extendedGrid.ColumnLength - 1, extendedGrid.RowLength - 1), outPath);
+    var totalRisk = outPath.Skip(1).Select(coord => extendedGrid[coord]).Sum();
+    //DrawSolution(extendedGrid);
+    Console.WriteLine($"Part B: Total risk: {totalRisk}");
+
+}
+
+void DrawSolution(Array2D<int> grid)
+{
+    for (int row = 0; row < grid.RowLength; ++row)
     {
         for (int col = 0; col < grid.ColumnLength; ++col)
         {
--- 2021/C#/Library/Algorithms.cs
+++ 2021/C#/Library/Algorithms.cs
@@ -1,21 +1,30 @@
 ﻿using Library.Datastructures;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Library.Algorithms
 {
     public static class Algorithms
     {
-        private struct AStarNode : IEquatable<AStarNode>
+        private struct AStarNode
         {
             public int parentIndex;
             public int selfIndex;
             public int startCost;
             public int endCost;
             public int cost => startCost + endCost;
+        }
 
-            public bool Equals(AStarNode other)
+        private class AStarNodeComparer : IComparer<AStarNode>
+        {
+            public int Compare(AStarNode x, AStarNode y)
             {
-                return selfIndex == other.selfIndex;
+                var cost = x.cost - y.cost;
+                if (cost == 0)
+                {
+                    return x.selfIndex - y.selfIndex;
+                }
+                return cost;
             }
         }
 
@@ -24,15 +33,15 @@ namespace Library.Algorithms
             var startIndex = weightTable.RowColumnToIndex(start);
             var endIndex = weightTable.RowColumnToIndex(end);
             var startNode = new AStarNode { startCost = 0, endCost = end.GetManhattanDistance(start), parentIndex = startIndex, selfIndex = startIndex};
-            List<AStarNode> openList = new List<AStarNode> { startNode };
-            List<int> closedIndexList = new List<int> ();
+            SortedSet<AStarNode> openList = new SortedSet<AStarNode>(new AStarNodeComparer()) { startNode };
+            HashSet<int> closedIndexList = new HashSet<int> ();
             Array2D<AStarNode> nodeGrid = new Array2D<AStarNode>(weightTable.RowLength, weightTable.ColumnLength, Enumerable.Repeat(new AStarNode {  startCost = -1 }, weightTable.Length).ToArray());
             nodeGrid[start] = startNode;
             int[] neighbourIndexCache = new int[4];
 
             while(openList.Count > 0)
             {
-                AStarNode lowestCost = openList.MinBy(x => x.cost);
+                AStarNode lowestCost = openList.First();
                 openList.Remove(lowestCost);
                 closedIndexList.Add(lowestCost.selfIndex);
 
@@ -60,7 +69,7 @@ namespace Library.Algorithms
                         neighbourNode.endCost = weightTable.IndexToRowColumn(neighbourIndex).GetManhattanDistance(end);
                         if (nodeGrid[neighbourIndex].cost < 0 || nodeGrid[neighbourIndex].cost > neighbourNode.cost)
                         {
-                            openList.Add(neighbourNode);
+                            var success = openList.Add(neighbourNode);
                             nodeGrid[neighbourIndex] = neighbourNode;
                         }
                     }
