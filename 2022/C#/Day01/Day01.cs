987bce2b23d4eef38ce574c6d45d4484c39027c8
Pieter Vantorre <pietervantorre@gmail.com>
2022-12-02 00:46:21 +0100

    Added 2022 project + day 1 solution

---
+++ 2022/C#/Day01/Day01.cs
@@ -0,0 +1,25 @@
+﻿using Library;
+
+var calories = IO.ReadInputAsStringArray(false);
+// unfortunately i'm too rusty to find an elegent LINQ algorithm for this. so let's go the oldschool way!
+var caloriesPerElf = new List<int>();
+caloriesPerElf.Add(0);
+int index = 0;
+foreach(var calory in calories)
+{
+    if (string.IsNullOrEmpty(calory))
+    {
+        index++;
+        caloriesPerElf.Add(0);
+        continue;
+    }
+    caloriesPerElf[index] += int.Parse(calory);
+}
+Console.WriteLine("Part one");
+var maxCaloriesOnOneElf = caloriesPerElf.Max();
+Console.WriteLine($"Max calories on 1 elf: {maxCaloriesOnOneElf}");
+
+Console.WriteLine("Part two");
+caloriesPerElf.Sort();
+var maxCaloriesOnTop3Elfs = caloriesPerElf.TakeLast(3).Sum();
+Console.WriteLine($"Max calories on top 3 elfs: {maxCaloriesOnTop3Elfs}");
---
+++ 2022/C#/Library/Extensions.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+
+namespace Library.Extensions
+{
+    public static class Extensions
+    {
+        public static RangeEnumerator GetEnumerator(this Range range)
+        {
+            if (range.Start.IsFromEnd || range.End.IsFromEnd)
+            {
+                throw new ArgumentException(nameof(range));
+            }
+
+            return new RangeEnumerator(range.Start.Value, range.End.Value);
+        }
+
+        public struct RangeEnumerator : IEnumerator<int>
+        {
+            private readonly int _end;
+            private int _current;
+            private bool countUp;
+
+            public RangeEnumerator(int start, int end)
+            {
+                _end = end;
+                countUp = end >= start;
+                if (countUp)
+                {
+                    _current = start - 1;
+                }
+                else
+                {
+                    _current = start + 1;
+                }
+            }
+
+            public int Current => _current;
+            object IEnumerator.Current => Current;
+
+            public bool MoveNext()
+            {
+                if (countUp)
+                {
+                    return ++_current < _end;
+                }
+                else
+                {
+                    return --_current > _end;
+                }
+            }
+
+            public void Dispose() { }
+            public void Reset()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
