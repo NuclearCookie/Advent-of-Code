1f8176b3938609e00c063ae38d16547729a3447e
Pieter Vantorre <pietervantorre@gmail.com>
2022-12-06 23:27:54 +0100

    Day 6

--- 2022/C#/Day06/Day06.cs
+++ 2022/C#/Day06/Day06.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using Library;
+
+var dataStream = IO.ReadInputAsString(false);
+var markerPos = FindSequentialUniqueCharacters(4);
+var messagePos = FindSequentialUniqueCharacters(14);
+
+Console.WriteLine($"Marker position found at index {markerPos}");
+Console.WriteLine($"Message position found at index {messagePos}");
+
+int FindSequentialUniqueCharacters(int count)
+{
+    for (int i = count - 1; i < dataStream.Length; i++)
+    {
+        var lastFourChars = dataStream.AsSpan(i - (count - 1), count);
+        if (lastFourChars.ToArray().Distinct().Count() == count)
+        {
+            return i + 1;
+        }
+    }
+    return 0;
+}
