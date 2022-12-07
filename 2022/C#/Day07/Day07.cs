7ac9849f8d749bdecf14a9b40fbe0769017af34c
Pieter Vantorre <pietervantorre@gmail.com>
2022-12-07 14:04:04 +0100

    Day 7

--- 2022/C#/Day07/Day07.cs
+++ 2022/C#/Day07/Day07.cs
@@ -1,1 +1,100 @@
-﻿
+﻿using Library;
+using Day07;
+using System.Diagnostics;
+
+var commands = IO.ReadInputAsString(false).Split('$', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(command => command.Split(Environment.NewLine));
+Day07.DirectoryInfo rootDirectory = new("/", null);
+Day07.DirectoryInfo currentDirectory = rootDirectory;
+
+foreach(var command in commands.Skip(1))
+{
+    if (command[0].StartsWith("cd"))
+    {
+        ChangeDirectory(command);
+    }
+    else if (command[0].StartsWith("ls"))
+    {
+        PopulateDirectory(command);
+    }
+}
+
+List<Day07.DirectoryInfo> topDirsUnderThreshold = new();
+RecursiveFindDirectoriesUnderThreshold(rootDirectory, 100000, topDirsUnderThreshold);
+
+Console.WriteLine($"Total size of directories under threshold: {topDirsUnderThreshold.Aggregate(0, (sum, current) => current.ComputeSize() + sum)}");
+
+var totalSizeInUse = rootDirectory.ComputeSize();
+var totalSizeToClear = 30000000 - (70000000 - totalSizeInUse);
+List<Day07.DirectoryInfo> allDirsOverThreshold = new();
+RecursiveFindDirectoriesOverThreshold(rootDirectory, totalSizeToClear, allDirsOverThreshold);
+allDirsOverThreshold.Sort((a, b) => a.ComputeSize() - b.ComputeSize());
+Console.WriteLine($"The best directory to clear is {allDirsOverThreshold[0].Name}, with a size of {allDirsOverThreshold[0].ComputeSize()}");
+
+void ChangeDirectory(string[] command)
+{
+    Debug.Assert(command.Length == 1, "Unhandled CD command");
+    var dir = command[0].Substring(3);
+    if (dir == "..")
+    {
+        currentDirectory = currentDirectory.Parent;
+    }
+    else
+    {
+        var existingDir = currentDirectory.Directories.Find(subDir => subDir.Name.Equals(dir));
+        if (existingDir == null)
+            currentDirectory = new Day07.DirectoryInfo(dir, currentDirectory);
+        else
+            currentDirectory = existingDir;
+    }
+}
+
+void PopulateDirectory(string[] command)
+{
+    for(int i = 1; i < command.Length; ++i)
+    {
+        var entry = command[i].Split(' ');
+        if (entry[0].StartsWith("dir"))
+        {
+            var dir = entry[1];
+            var existingDir = currentDirectory.Directories.Find(subDir => subDir.Name.Equals(dir));
+            if (existingDir != null)
+                continue;
+            new Day07.DirectoryInfo(dir, currentDirectory);
+        }
+        else
+        {
+            var size = int.Parse(entry[0]);
+            var name = entry[1];
+            var existingFile = currentDirectory.Files.Find(file => file.Name.Equals(name));
+            if (existingFile != null)
+                continue;
+            currentDirectory.Files.Add(new Day07.File { Name = name, Size = size });
+        }
+    }
+}
+
+void RecursiveFindDirectoriesUnderThreshold(Day07.DirectoryInfo currentDirectory, int threshold, List<Day07.DirectoryInfo> outputList)
+{
+    var dirSize = currentDirectory.ComputeSize();
+    if (dirSize <= threshold)
+    {
+        outputList.Add(currentDirectory);
+    }
+    foreach(var directory in currentDirectory.Directories)
+    {
+        RecursiveFindDirectoriesUnderThreshold(directory, threshold, outputList);
+    }
+}
+
+void RecursiveFindDirectoriesOverThreshold(Day07.DirectoryInfo currentDirectory, int threshold, List<Day07.DirectoryInfo> outputList)
+{
+    var dirSize = currentDirectory.ComputeSize();
+    if (dirSize > threshold)
+    {
+        outputList.Add(currentDirectory);
+    }
+    foreach(var directory in currentDirectory.Directories)
+    {
+        RecursiveFindDirectoriesOverThreshold(directory, threshold, outputList);
+    }
+}
---
+++ 2022/C#/Day07/DirectoryInfo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day07
+{
+    internal class DirectoryInfo
+    {
+        public DirectoryInfo(string name, DirectoryInfo? parent)
+        {
+            Name = name;
+            Parent = parent;
+            if (Parent != null)
+            {
+                Parent.Directories.Add(this);
+            }
+        }
+
+        public bool Contains(string name, bool isDirectory)
+        {
+            if (isDirectory)
+            {
+                var found = Directories.Find(dir => dir.Name.Equals(name));
+                return found != null;
+            }
+            else
+            {
+                var found = Files.Find(file => file.Name.Equals(name));
+                return found != null;
+            }
+            return false;
+        }
+
+        public int ComputeSize()
+        {
+            int count = 0;
+            foreach(var file in Files)
+            {
+                count += file.Size;
+            }
+            foreach(var dir in Directories)
+            {
+                count += dir.ComputeSize();
+            }
+            return count;
+        }
+
+        public List<DirectoryInfo> Directories = new List<DirectoryInfo>();
+        public List<File> Files = new List<File>();
+        public DirectoryInfo? Parent = null;
+        public string Name;
+    }
+
+    internal class File
+    {
+        public string Name;
+        public int Size;
+    }
+}
